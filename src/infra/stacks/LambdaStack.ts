8efc71ad242937b3174a58b755acd68907959065
luissantiago <luis-santiago97@hotmail.com>
2024-10-26 17:51:24 -0500

    first commit

---
+++ src/infra/stacks/LambdaStack.ts
@@ -0,0 +1,22 @@
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function as Lambda, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import { join } from 'path';
+
+
+export class LambdaStack extends Stack {
+  public readonly helloLambdaIntegration: LambdaIntegration
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    const helloLambda = new Lambda(this, 'hello-lambda', {
+      runtime: Runtime.NODEJS_20_X,
+      handler: 'hello.main',
+      code: Code.fromAsset(join(__dirname, '..', '..', 'services'))
+    })
+
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
+  }
+}
